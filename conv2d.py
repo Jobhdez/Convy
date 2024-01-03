aaa27efeb7b9da3cfb5973cc16ecdc104af6d2e3
Job Hernandez Lara <hj93@protonmail.com>
2024-01-02 18:22:58 -0800

    add a new example

--- conv2d.py
+++ conv2d.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 import numpy
+import torch.nn.functional as F
 
 class Net(nn.Module):
     def __init__(self):
@@ -117,4 +118,74 @@ output:
 torch output: tensor([[[[1.1876]]]]) 
 
 numpy_output: [[[[1.18756902]]]]
+"""
+#### word embdeeings
+CONTEXT_SIZE = 2
+EMBEDDING_DIM = 10
+# We will use Shakespeare Sonnet 2
+test_sentence = """When forty winters shall besiege thy brow,
+And dig deep trenches in thy beauty's field,
+Thy youth's proud livery so gazed on now,
+Will be a totter'd weed of small worth held:
+Then being asked, where all thy beauty lies,
+Where all the treasure of thy lusty days;
+To say, within thine own deep sunken eyes,
+Were an all-eating shame, and thriftless praise.
+How much more praise deserv'd thy beauty's use,
+If thou couldst answer 'This fair child of mine
+Shall sum my count, and make my old excuse,'
+Proving his beauty by succession thine!
+This were to be new made when thou art old,
+And see thy blood warm when thou feel'st it cold.""".split()
+# we should tokenize the input, but we will ignore that for now
+# build a list of tuples.
+# Each tuple is ([ word_i-CONTEXT_SIZE, ..., word_i-1 ], target word)
+ngrams = [
+    (
+        [test_sentence[i - j - 1] for j in range(CONTEXT_SIZE)],
+        test_sentence[i]
+    )
+    for i in range(CONTEXT_SIZE, len(test_sentence))
+]
+# Print the first 3, just so you can see what they look like.
+print(ngrams[:3])
+
+vocab = set(test_sentence)
+word_to_ix = {word: i for i, word in enumerate(vocab)}
+
+
+class NGramLanguageModeler(nn.Module):
+
+    def __init__(self, vocab_size, embedding_dim, context_size):
+        super(NGramLanguageModeler, self).__init__()
+        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.linear1 = nn.Linear(context_size * embedding_dim, 128)
+        self.linear2 = nn.Linear(128, vocab_size)
+
+    def forward(self, inputs):
+        embeds = self.embeddings(inputs).view((1, -1))
+        out = F.relu(self.linear1(embeds))
+        out = self.linear2(out)
+        log_probs = F.log_softmax(out, dim=1)
+        return log_probs
+
+vocab_size = 10000
+embedding_dim = 50
+context_size = 2
+
+# Create a random input tensor with the shape (batch_size, context_size)
+batch_size = 1  # You can change this based on your actual use case
+input_tensor = torch.randint(low=0, high=vocab_size, size=(batch_size, context_size))
+
+# Instantiate the model
+model2 = NGramLanguageModeler(vocab_size, embedding_dim, context_size)
+
+# Trace the model with the example input tensor
+traced_model = torch.jit.trace(model2, input_tensor)
+
+"""
+>>> from compiler import get_node_inputs
+>>> inpsf = get_node_inputs(traced_model)
+>>> inpsf
+['__torch__.conv2d.NGramLanguageModeler', '__torch__.conv2d.NGramLanguageModeler', '__torch__.conv2d.NGramLanguageModeler', '__torch__.torch.nn.modules.sparse.Embedding', 'Long(1, 2, strides=[2, 1], requires_grad=0, device=cpu)', 'int', 'int', 'Tensor', 'int[]', '__torch__.torch.nn.modules.linear.Linear', 'Float(1, 100, strides=[100, 1], requires_grad=1, device=cpu)', 'Tensor', '__torch__.torch.nn.modules.linear.___torch_mangle_2.Linear', 'Float(1, 128, strides=[128, 1], requires_grad=1, device=cpu)', 'Tensor', 'int', 'NoneType']
 """
