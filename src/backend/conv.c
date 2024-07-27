02f686a448125bf01e11f8af1e6fdc2819855023
Job Hernandez <hj93@protonmail.com>
2024-07-26 18:45:20 -0700

    fix indentation errors in the generated code

--- src/backend/conv.c
+++ src/backend/conv.c
@@ -1,38 +1,22 @@
+
 #include "runtime.c"
 #include <stdio.h>
 
-
 int main() {
+    int batch_size = 1;
+    int channels = 1;
+    int input_height = 3;
+    int input_width = 3;
+    int filter_height = 3;
+    int filter_width = 3;
+    float input_data[1][1][3][3] = {{{{1.0, 1.0, 1.0}, {1.0, 1.0, 1.0}, {1.0, 1.0, 1.0}}}};
+    float weight[1][1][3][3] = {{{{0.1206025704741478, 0.2804994285106659, 0.10350386798381805}, {-0.31444603204727173, -0.3109368085861206, -0.22286033630371094}, {-0.12215566635131836, 0.2765805721282959, 0.25621065497398376}}}};
+    float bias = -0.24808375537395477;
+    float output[1][1][3][3];
 
-int batch_size = 1;
+    convolution(input_data, weight, &bias, batch_size, channels, input_height, input_width, filter_height, filter_width, output);
 
-int channels = 1;
-
-int input_height = 3;
-
-int input_width = 3;
-
-int filter_height = 3;
-
-int filter_width = 3;
-
-float input_data[1][1][3][3] = {{{{1.0, 1.0, 1.0}, {1.0, 1.0, 1.0}, {1.0, 1.0, 1.0}}}};
-
-float weight[1][1][3][3] = {{{{0.113400898873806, 0.28415676951408386, -0.16677796840667725}, {0.022547762840986252, 0.24742528796195984, 0.010448455810546875}, {-0.1895950734615326, -0.05162879079580307, -0.09433500468730927}}}};
-
-float bias = -0.014137348160147667;
-float output[1][1][3][3];
-
-convolution(input_data, weight, &bias, batch_size, channels, input_height, input_width, filter_height, filter_width, output);
-
-for (int i = 0; i < batch_size; ++i) {
-  for (int j = 0; j < 1; ++j) {
-     for (int k = 0; k < 3; ++k) {
-       for (int l = 0; l < 3; ++l) {
-         printf("%f ", output[i][j][k][l]);
-       }
-     }
-  }
-}
-return 0;
+    printf("%f", output[0][0][0][0]);
+       
+    return 0;
 }
--- src/backend/lalg_to_c.py
+++ src/backend/lalg_to_c.py
@@ -1,16 +1,15 @@
 import torch
-#from src.frontend.torchfx import Conv2dNode
 import torch.fx
 from src.frontend.nodes import Conv2dNode
             
 def to_c(node):
-    print(f'hello: {type(node)}')
     if isinstance(node, Conv2dNode):
-            
-        c_str = ""
-        c_str = c_str = '#include "runtime.c"\n'
-        c_str = c_str + "#include <stdio.h>\n\n"
+        c_str = """
+#include "runtime.c"
+#include <stdio.h>
 
+int main() {
+"""
         bias = str(node.bias)
         input_h = str(node.input_height)
         input_w = str(node.input_width)
@@ -22,39 +21,29 @@ def to_c(node):
         tensor = torch_tensor_to_c(node.input_tensor)
         weight = torch_tensor_to_c(node.weight)
         
-        c_str = c_str + "\nint main() {\n\n"
-        c_str = c_str + f'int batch_size = {str(int(batch_s))};\n\n'
-        c_str = c_str + f'int channels = {str(int(channels))};\n\n'
-        c_str = c_str + f'int input_height = {str(int(input_h))};\n\n'
-        c_str = c_str + f'int input_width = {str(int(input_w))};\n\n'
-        c_str = c_str + f'int filter_height = {str(int(filter_h))};\n\n'
-        c_str = c_str + f'int filter_width = {str(int(filter_w))};\n\n'
+        c_str += f"    int batch_size = {str(int(batch_s))};\n"
+        c_str += f"    int channels = {str(int(channels))};\n"
+        c_str += f"    int input_height = {str(int(input_h))};\n"
+        c_str += f"    int input_width = {str(int(input_w))};\n"
+        c_str += f"    int filter_height = {str(int(filter_h))};\n"
+        c_str += f"    int filter_width = {str(int(filter_w))};\n"
+        
+        c_str += f"    float input_data[{batch_s}][{channels}][{input_h}][{input_w}] = {tensor};\n"
+        c_str += f"    float weight[{batch_s}][{channels}][{filter_h}][{filter_w}] = {weight};\n"
+        c_str += f"    float bias = {bias};\n"
+        c_str += f"    float output[{batch_s}][{channels}][{filter_h}][{filter_h}];\n"
+        
+        c_str += """
+    convolution(input_data, weight, &bias, batch_size, channels, input_height, input_width, filter_height, filter_width, output);
 
-        c_str = c_str + f'float input_data[{batch_s}][{channels}][{input_h}][{input_w}] = {tensor};\n\n'
-        c_str = c_str + f'float weight[{batch_s}][{channels}][{filter_h}][{filter_w}] = {weight};\n\n'
-        c_str = c_str + f'float bias = {bias};\n'
-        c_str = c_str + f'float output[{batch_s}][{channels}][{filter_h}][{filter_h}];\n\n'
-        c_print = "convolution(input_data, weight, &bias, batch_size, channels, input_height, input_width, filter_height, filter_width, output);\n\n"
-        c_print = c_print + "for (int i = 0; i < batch_size; ++i) {\n"
-        c_print = c_print + "  for (int j = 0; j < 1; ++j) {\n"
-        c_print = c_print + "     for (int k = 0; k < 3; ++k) {\n"
-        c_print = c_print + "       for (int l = 0; l < 3; ++l) {\n"
-        c_print = c_print + '         printf("%f ", output[i][j][k][l]);\n'
-        c_print = c_print + "       }\n"
-        c_print = c_print + "     }\n"
-        c_print = c_print + "  }\n"
-        c_print = c_print + "}\n"
-   
-        c_str = c_str + c_print + 'return 0;\n}'
+    printf("%f", output[0][0][0][0]);
+       
+    return 0;
+}
+"""
         return c_str
 
-
-                
 def torch_tensor_to_c(tensor):
-    c_array = tensor.numpy()
-    c_array = c_array.tolist()
-    c_array = str(c_array)
-    print(c_array)
-    c_array = c_array.replace('[', '{').replace(']', '}')
+    c_array = tensor.numpy().tolist()
+    c_array = str(c_array).replace('[', '{').replace(']', '}')
     return c_array
-
